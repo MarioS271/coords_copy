59fe188e2022fc7661624d3b822bad1d50d2517e
MarioS271 <marioerik.scholze@gmail.com>
2026-08-02 02:02:38 +0200

    fix: change configdata log strings from "CatVision" to MOD_FRIENDLY_NAME

--- src/main/java/net/marios271/coords_copy/config/ConfigData.java
+++ src/main/java/net/marios271/coords_copy/config/ConfigData.java
@@ -2,7 +2,6 @@ package net.marios271.coords_copy.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import net.fabricmc.loader.api.FabricLoader;
 import net.marios271.coords_copy.CoordsCopy;
 
 import java.io.File;
@@ -20,7 +19,7 @@ public class ConfigData {
 	public void save() {
 		try (FileWriter writer = new FileWriter(file)) {
 			GSON.toJson(this, writer);
-			CoordsCopy.LOGGER.info("Saved CatVision config");
+			CoordsCopy.LOGGER.info("Saved " + CoordsCopy.MOD_FRIENDLY_NAME + " config");
 		} catch (IOException exception) {
 			CoordsCopy.LOGGER.error("Failed to save config", exception);
 		}
@@ -31,7 +30,7 @@ public class ConfigData {
 		ConfigData result = null;
 		if (file.exists()) {
 			try (FileReader reader = new FileReader(file)) {
-				CoordsCopy.LOGGER.info("Loaded CatVision config");
+				CoordsCopy.LOGGER.info("Loaded " + CoordsCopy.MOD_FRIENDLY_NAME + " config");
 				result = GSON.fromJson(reader, ConfigData.class);
 			} catch (IOException exception) {
 				CoordsCopy.LOGGER.warn("Failed to load config, returning default values");
