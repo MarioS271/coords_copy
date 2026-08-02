8ae4d95351b866a0f1a8e2675fed975406268aad
MarioS271 <marioerik.scholze@gmail.com>
2026-08-02 18:35:44 +0200

    feat(config-screen): changed from display in chat instead of actionbar (true/false) to display location (actionbar/chat) choice in onfig

--- src/main/java/net/marios271/coords_copy/config/ConfigScreen.java
+++ src/main/java/net/marios271/coords_copy/config/ConfigScreen.java
@@ -3,9 +3,11 @@ package net.marios271.coords_copy.config;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import me.shedaniel.clothconfig2.gui.entries.SelectionListEntry;
 import net.minecraft.client.gui.screens.Screen;
 //? < 1.19 {
-/*import net.minecraft.network.chat.TranslatableComponent;
+/*import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 *///?} else {
 import net.minecraft.network.chat.Component;
 //?}
@@ -34,18 +36,29 @@ public class ConfigScreen {
         // Show in Chat instead of Actionbar
         category.addEntry(entryBuilder.startBooleanToggle(
 			//? < 1.19 {
-			/*new TranslatableComponent("text.coords_copy.config.option.chat_instead_of_actionbar"),
+			/*new TranslatableComponent("text.coords_copy.config.option.message_location"),
 			*///?} else {
-			Component.translatable("text.coords_copy.config.option.chat_instead_of_actionbar"),
+			Component.translatable("text.coords_copy.config.option.message_location"),
 			//?}
 			config.chat_instead_of_actionbar
 		)
             .setDefaultValue(true)
             .setSaveConsumer(new_value -> config.chat_instead_of_actionbar = new_value)
+			.setYesNoTextSupplier(ConfigScreen::messageLocationYesNoTextSupplier)
             .build());
 
         builder.setSavingRunnable(config::save);
 
         return builder.build();
     }
+
+	private static Component messageLocationYesNoTextSupplier(boolean value) {
+		//? < 1.19 {
+		/*if (value) return new TranslatableComponent("text.coords_copy.config.option.message_location.value.chat");
+		else return new TranslatableComponent("text.coords_copy.config.option.message_location.value.actionbar");
+		*///?} else {
+		if (value) return Component.translatable("text.coords_copy.config.option.message_location.value.chat");
+		else return Component.translatable("text.coords_copy.config.option.message_location.value.actionbar");
+		//?}
+	}
 }
